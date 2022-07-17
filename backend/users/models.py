b9062e3aac14d2b691c63cb2d46a18786ed10505
Dhritiman Bharadwaj <dhritimanbhardwaj@gmail.com>
2022-07-17 14:11:23 +0530

    student data api creation

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,11 +1,28 @@
 import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
 # Create your models here.
+
+
+class CustomUser(AbstractUser):
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_professor = models.BooleanField(default=False)
+
+class Professor(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    gender = models.CharField(max_length=50)
+    dob = models.DateField()
+    father_name = models.CharField(max_length=50)
+    mother_name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    nationality = models.CharField(max_length=50)
 
 class Student(models.Model):
     id = models.UUIDField(primary_key=True, default= uuid.uuid4)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)   
     registration_number = models.CharField(max_length=15)
     name = models.CharField(max_length=50)
     email = models.EmailField()
@@ -17,17 +34,6 @@ class Student(models.Model):
     address = models.CharField(max_length=50)
     nationality = models.CharField(max_length=50)
 
-class Professor(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.EmailField()
-    gender = models.CharField(max_length=50)
-    dob = models.DateField()
-    father_name = models.CharField(max_length=50)
-    mother_name = models.CharField(max_length=50)
-    address = models.CharField(max_length=50)
-    nationality = models.CharField(max_length=50)
-
-class CustomUser(AbstractUser):
-    is_superuser = models.BooleanField(default=False)
-    is_staff = models.BooleanField(default=False)
-    is_professor = models.BooleanField(default=False)
+    def save(self, *args, **kwargs):
+        self.user = CustomUser.objects.create_user(username=self.registration_number, password='abcdefg')
+        return super().save(*args, **kwargs)
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from users.views import StudentDetailsAPI
+
 urlpatterns = [
-    
+    path('student/',StudentDetailsAPI.as_view()),
 ]
