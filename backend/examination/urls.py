6da7e65946463ae0183333ffc35208218f0c33d5
Dhritiman Bharadwaj <dhritimanbhardwaj@gmail.com>
2022-07-17 16:19:12 +0530

    minor fixes

--- backend/examination/urls.py
+++ backend/examination/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ExamAPIView, StudentAnswersheetAPIview
+from .views import AnswerSheetAPIView, ExamAPIView, StudentAnswersheetAPIview
 
 urlpatterns = [
     path("exam/", ExamAPIView.as_view()),
-    path("answersheet/",StudentAnswersheetAPIview.as_view())
+    path("answersheet/",AnswerSheetAPIView.as_view()),
+    path('student-sheet/', StudentAnswersheetAPIview.as_view())
 ]
